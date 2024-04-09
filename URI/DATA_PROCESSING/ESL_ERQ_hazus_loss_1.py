f77e625c317ee895644bc9b88484146bdbb5e1d5
Shweta2415 <Shweta.Sharma2415@gmail.com>
2024-04-09 18:15:35 +1000

    Changed Data source in ESL_ERQ_hazus_loss_1.py file and ESL_FLD_hazus_loss_1.py

--- URI/DATA_PROCESSING/ESL_ERQ_hazus_loss_1.py
+++ URI/DATA_PROCESSING/ESL_ERQ_hazus_loss_1.py
@@ -1,32 +1,40 @@
-""" incorporate flooding damage due to earthquakes"""
+""" incorporate flooding damaage due to coastal storms"""
 
 #%% read packages
+import numpy as np
+import pandas as pd
 import geopandas as gpd
 import os
+import matplotlib.pyplot as plt
+from shapely.ops import nearest_points
+import requests
+
 import URI.MISC.params_1 as params
 import URI.MISC.utils_1 as utils
 import URI.MISC.plotting_1 as plotting
+import URI.MISC.plotting_1 as plotting
 utils.set_home()
-
 #%% EXTRACT PARAMETERS
 # Input paths
-path_gbd = params.PATHNAMES.at['ESL_ERQ_hazus_gbd', 'Value']
-layer_gbd = params.PATHNAMES.at['ESL_ERQ_hazus_layer', 'Value']
+# path_gbd = params.PATHNAMES.at['ESL_ERQ_hazus_gbd', 'Value']
+# layer_gbd = params.PATHNAMES.at['ESL_ERQ_hazus_layer', 'Value']
 # Output paths
 path_output = params.PATHNAMES.at['ESL_ERQ_hazus_loss', 'Value']
 
 #%% LOAD DATA
 gdf_tract = utils.get_blank_tract(add_pop=True)
-df_hazus = gpd.read_file(path_gbd, driver='FileGBD', layer=layer_gbd)
+# df_hazus = gpd.read_file(path_gbd, driver='FileGBD', layer=layer_gbd)
+df_hazus = gpd.read_file(r".\\1_RAW_INPUTS\Hazus - Loss Estimation Data\Earthquakes (5.2M)\results.shp")
+
 
 #%% use tract to merge hazus data to tract
-gdf_tract = gdf_tract.merge(df_hazus[['Tract', 'TotalLoss']], left_on='geoid', right_on='Tract', how='left')
+gdf_tract = gdf_tract.merge(df_hazus[['tract', 'EconLoss']], left_on='geoid', right_on='tract', how='left')
 
 #%% convert to current USD
-gdf_tract.TotalLoss = utils.convert_USD(gdf_tract.TotalLoss.values, 2018)
+gdf_tract.EconLoss = utils.convert_USD(gdf_tract.EconLoss.values, 2018)
 
 #%% total loss is in USD 1000.... convert
-gdf_tract['Loss_USD'] = gdf_tract['TotalLoss'] * 1000.
+gdf_tract['Loss_USD'] = gdf_tract['EconLoss'] * 1000.
 
 #%% save as output
 gdf_tract.to_file(path_output)
